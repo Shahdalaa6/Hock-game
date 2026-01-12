b50f7bc027372b667958ad3028d02c39003e66ce
Shahdalaa6 <user@example.com>
2026-01-12 20:18:27 +0200

    Remove 3-player restriction and enable instant game start4

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ let currentPlayerName = "";
 
 /* ---------------- SCREENS ---------------- */
 
-function showScreen(id) {
-    document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
-    document.getElementById(id).classList.add("active");
-}
+// function showScreen(id) {
+//     document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
+//     document.getElementById(id).classList.add("active");
+// }
 
 /* ---------------- START ---------------- */
 
